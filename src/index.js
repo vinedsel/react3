a5368070cdb6c3d2b7600ee38cbb60f5575b9a1c
Raymond Gregory <vinedsel@gmail.com>
2018-12-13 15:25:41 -0600

    Practice code involving component-making.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const Grocerylist = props => {
+  return (
+    <ul>
+      <li>{props.item1}</li>
+      <li>{props.item2}</li>
+      <li>{props.item3}</li>
+    </ul>
+  );
+};
+
+ReactDOM.render(
+  <Grocerylist item1="Apples" item2="Oranges" item3="Mangoes" />,
+  document.getElementById('root')
+);
