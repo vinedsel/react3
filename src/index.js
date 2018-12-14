4ec361a428a6087126ce61dfdf5d3065a9f468a9
Raymond Gregory <vinedsel@gmail.com>
2018-12-14 16:20:54 -0600

    Practice code involving main component-making.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Grocerylist = props => {
-  return (
-    <ul>
-      <li>{props.item1}</li>
-      <li>{props.item2}</li>
-      <li>{props.item3}</li>
-    </ul>
+// regular component
+
+// const Grocerylist = props => {
+//   return (
+//     <ul>
+//       <li>{props.item1}</li>
+//       <li>{props.item2}</li>
+//       <li>{props.item3}</li>
+//     </ul>
+//   );
+// };
+//
+// ReactDOM.render(
+//   <Grocerylist item1="Apples" item2="Oranges" item3="Mangoes" />,
+//   document.getElementById('root')
+// );
+
+
+// main component
+
+const App = props => {
+  return(
+    <div>
+      <Hello name="Nikki" />
+      <Hello name="Michael" />
+      <Hello name="Andrew" />
+    </div>
   );
 };
 
+const Hello = props => (
+  <div>
+    <h1>Hello, {props.name}!</h1>
+  </div>
+);
+
 ReactDOM.render(
-  <Grocerylist item1="Apples" item2="Oranges" item3="Mangoes" />,
+  <App/>,
   document.getElementById('root')
 );
