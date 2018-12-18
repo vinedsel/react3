370e3aaed4a41394755cd3262c21c9a2a16633b6
Raymond Gregory <vinedsel@gmail.com>
2018-12-18 15:14:54 -0600

    Modded code.

--- src/index.js
+++ src/index.js
@@ -83,3 +83,26 @@ import PropTypes from 'prop-types';
 //_____________________________________________________________________________
 
 // Rendering Objects with the Map Function
+
+const App = props => {
+  const items = [
+    { name: 'Bread', price: 2.35 },
+    { name: 'Milk', price: 2.0 },
+    { name: 'Eggs', price: 3.15 },
+    { name: 'Tea', price: 4.0 }
+  ];
+
+  return <List items={items} />;
+};
+
+const List = props => {
+  const listItems = props.items.map((item, index) => (
+    <li key={index}>
+      {item.name}: {item.price}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
